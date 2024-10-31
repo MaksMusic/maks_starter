99e1fccf612fa23e999629c6397729b81f1c2f8b
MaksMusic <Maksmusic05@yandex.ru>
2024-10-31 14:12:33 +0300

    git tag v0.1.3

--- src/main/java/ru/maksmusic/maks_starter/MaksStarterApplication.java
+++ src/main/java/ru/maksmusic/maks_starter/MaksStarterApplication.java
@@ -2,8 +2,10 @@ package ru.maksmusic.maks_starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class MaksStarterApplication {
 
     public static void main(String[] args) {
--- src/main/java/ru/maksmusic/maks_starter/service_bot/BotServiceProperties.java
+++ src/main/java/ru/maksmusic/maks_starter/service_bot/BotServiceProperties.java
@@ -2,7 +2,9 @@ package ru.maksmusic.maks_starter.service_bot;
 
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @ConfigurationProperties(prefix = "bot.service") // Префикс для свойств
 public class BotServiceProperties {
     private String message = "Hello from MyService!";
